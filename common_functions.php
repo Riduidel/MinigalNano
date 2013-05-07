b656ba31b3b42d7ffc1605bff9f0e13aec52251f
Nicolas Delsaux <nicolas.delsaux@gmail.com>
2013-05-07 21:04:37 +0100

    Improved sanitize method by allowing better accents removall (hail to portugese accenting :-) )

--- common_functions.php
+++ common_functions.php
@@ -1,4 +1,7 @@
 <?php
+define("SANITIZE_TEST", "test_for_sanitize");
+define("REMOVE_ACCENTS_TEST", "test_for_remove_accents");
+
 function str_split_php4( $text, $split = 1 ) {
     // place each character of the string into and array
     $array = array();
@@ -12,13 +15,37 @@ function str_split_php4( $text, $split = 1 ) {
     return $array;
 }
 
+function remove_accents($str)
+{
+    $from = array(
+        "á", "à", "â", "ã", "ä", "é", "è", "ê", "ë", "í", "ì", "î", "ï",
+        "ó", "ò", "ô", "õ", "ö", "ú", "ù", "û", "ü", "ç", "Á", "À", "Â",
+        "Ã", "Ä", "É", "È", "Ê", "Ë", "Í", "Ì", "Î", "Ï", "Ó", "Ò", "Ô",
+        "Õ", "Ö", "Ú", "Ù", "Û", "Ü", "Ç"
+    );
+    $to = array(
+        "a", "a", "a", "a", "a", "e", "e", "e", "e", "i", "i", "i", "i",
+        "o", "o", "o", "o", "o", "u", "u", "u", "u", "c", "A", "A", "A",
+        "A", "A", "E", "E", "E", "E", "I", "I", "I", "I", "O", "O", "O",
+        "O", "O", "U", "U", "U", "U", "C"
+    );
+    return str_replace($from, $to, $str);
+}
+
 function sanitize($name)
 {
     // Sanitize image filename (taken from http://iamcam.wordpress.com/2007/03/20/clean-file-names-using-php-preg_replace/ )
-	$fname=$name;
-	$replace="_";
-	$pattern="/([[:alnum:]_\.-]*)/";
-	$fname=str_replace(str_split_php4(preg_replace($pattern,$replace,$fname)),$replace,$fname);
-	return $fname;
+	$source = remove_accents($name);
+	$replace = "_";
+	$pattern = "/([[:alnum:]_\.-]*)/";
+	return str_replace(str_split_php4(preg_replace($pattern,$replace,$source)),$replace,$source);
+}
+
+if(array_key_exists(SANITIZE_TEST, $_GET)) {
+    echo "sanitize return " . sanitize($_GET[SANITIZE_TEST]);
+}
+
+if(array_key_exists(REMOVE_ACCENTS_TEST, $_GET)) {
+    echo "remove accents return " . remove_accents($_GET[REMOVE_ACCENTS_TEST]);
 }
 ?>
